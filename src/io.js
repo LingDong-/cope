993bbba2bf87a7d079601d78b196672c81b7ccea
Lingdong Huang <i@lingdong.works>
2019-10-15 23:26:55 -0400

    fix io bug

--- src/io.js
+++ src/io.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 function loadJSON(path){
-  return JSON.parse(fs.readFileSync(__dirname+path));
+  return JSON.parse(fs.readFileSync(__dirname+'/'+path));
 }
 function loadTSV(path){
-  return fs.readFileSync(__dirname+path).toString().split("\n").filter(x=>x.length).map(x=>x.split("\t").map(y=>isNaN(parseFloat(y))?y:parseFloat(y)));
+  return fs.readFileSync(__dirname+'/'+path).toString().split("\n").filter(x=>x.length).map(x=>x.split("\t").map(y=>isNaN(parseFloat(y))?y:parseFloat(y)));
 }
 function writeFile(path,text){
-  fs.writeFile(__dirname+path,text,function(){});
+  fs.writeFile(__dirname+'/'+path,text,function(){});
 }
 function copyFile(path0,path1){
-  fs.copyFileSync(__dirname+path0,__dirname+path1);
+  fs.copyFileSync(__dirname+'/'+path0,__dirname+'/'+path1);
 }
